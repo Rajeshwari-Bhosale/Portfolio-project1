0fe91c854b007a7b9da79249e2402b52bab8c7cd
Rajeshwari-Bhosale <bhosalerajeshwari015@gmail.com>
2026-07-05 12:56:51 +0530

    Initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Frontend Design",
+    "Modern Technologies",
+    "Responsive Websites",
+    "Creative Solutions",
+  ],
+  typeSpeed: 50,
+  loop: true,
+  showCursor: false,
+});
